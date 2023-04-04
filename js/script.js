6c26fd001a67cd336e5c17e6ec949da349e3888f
Soumita29 <129766980+Soumita29@users.noreply.github.com>
2023-04-04 12:05:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('active');
+}
